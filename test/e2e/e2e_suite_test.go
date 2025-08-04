a6e4f26c736edce65b0a2de769935958734c6df8
nagarajRPoojari <nagarajRPoojari>
2025-08-04 20:11:46 +0530

    [PATCH] trigger CI

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -39,7 +39,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "local/orangectl:latest"
+	projectImage = "np137270/orangectl:latest"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
